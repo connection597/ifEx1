2e5bbb340fca3525e1ab7c8385089874eff1ace9
connection579 <wlswn4118@naver.com>
2020-11-08 14:59:41 +0900

    if~else문 연습 1

---
+++ src/ifEx1/ifEx1.java
@@ -0,0 +1,14 @@
+package ifEx1;
+
+public class ifEx1 {
+
+	public static void main(String[] args) {
+		char gender = 'F';
+		if(gender == 'F') {
+			System.out.println("여성입니다");
+		}else {
+			System.out.println("남성입니다");
+		}
+	}
+
+}
